df2493d4835a3ff31fba0baf2154df78f663c111
Savith Tata <savithtata@gmail.com>
2021-08-29 16:06:35 -0400

    .

---
+++ client/src/components/ContainerAbout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContainerAbout = (props) => {
+  return <div className="container bg-black">{props.children}</div>;
+};
+
+export default ContainerAbout;
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,11 +1,135 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./About.css";
+
+import ContainerAbout from "../components/ContainerAbout";
+import Navbar from "../components/NavBar";
 
 const About = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <div class="StreamsHero-image relative bg-gradient-to-r from-purple-400 via-green-400 to-red-400 h-1/2">
+        <div class="StreamsHero-content">
+          <div class="StreamsHero-buttonContainer">
+           
+          </div>
+          <h1 class="StreamsHero-header">ABOUT  TEKJUMP</h1>
+          <p class="StreamsHero-blurb">
+            Encontre comunidades, conversas e inspirações do que você mais
+            adora.
+          </p>
         </div>
-    )
-}
+      </div>
+      <div class="cards2">
+      <ContainerAbout >
+        <div class="flex space-x-4 ">
+          <div class=" group border-4 border-blue-600 hover:bg-white hover:shadow-lg hover:border-transparent hover:animate-pulse hover:shadow-blue  relative group max-w-md mx-auto rounded-xl  overflow-hidden md:max-w-2xltransition duration-150 ease-in-out transform hover:scale-110 hover:shadow-inner   ">
+            <div class="md:flex">
+              <div class="md:flex-shrink-0">
+                <img
+                  class="h-48 w-full object-cover md:h-full md:w-48  "
+                  src="https://via.placeholder.com/350x150"
+                  alt="Man looking at item at a store"
+                ></img>
+              </div>
+              <div class="p-8">
+                <a
+                  href="#"
+                  class="capitalize font-black mt-1 text-lg leading-tight  font-large text-blue-200 group-hover:text-black hover:underline"
+                >
+                  Finding customers for your new business
+                </a>
+                <p class="mt-2 text-blue-200 group-hover:text-gray-500">
+                  Getting a new business off the ground is a lot of hard work.
+                  Here are five ideas you can use to find your first customers.
+                </p>
+              </div>
+            </div>
+          </div>
 
-export default About
+          <div class=" group border-4 border-yellow-500 hover:bg-white hover:shadow-yellow hover:border-transparent  relative group max-w-md mx-auto rounded-xl  overflow-hidden md:max-w-2xltransition duration-150 ease-in-out transform hover:scale-110   ">
+            <div class="md:flex">
+              <div class="md:flex-shrink-0">
+                <img
+                  class="h-48 w-full object-cover md:h-full md:w-48"
+                  src="https://via.placeholder.com/350x150"
+                  alt="Man looking at item at a store"
+                ></img>
+              </div>
+              <div class="p-8">
+                <a
+                  href="#"
+                  class="capitalize font-black mt-1 text-lg leading-tight font-large text-yellow-200 group-hover:text-black hover:underline"
+                >
+                  Finding customers for your new business
+                </a>
+                <p class="mt-2 text-yellow-200 group-hover:text-gray-500">
+                  Getting a new business off the ground is a lot of hard work.
+                  Here are five ideas you can use to find your first customers.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ContainerAbout>
+      </div>
+      <br></br>
+      <div class="cards3">
+      <ContainerAbout >
+        <div class="flex space-x-4 ">
+          <div class=" flex-1 group border-4 border-red-500 hover:bg-white hover:shadow-red hover:border-transparent  relative group max-w-md mx-auto rounded-xl  overflow-hidden md:max-w-2xltransition duration-150 ease-in-out transform hover:scale-110   ">
+            <div class="md:flex">
+              <div class="md:flex-shrink-0">
+                <img
+                  class="h-48 w-full object-cover md:h-full md:w-48"
+                  src="https://via.placeholder.com/350x150"
+                  alt="Man looking at item at a store"
+                ></img>
+              </div>
+              <div class="p-8">
+                <a
+                  href="#"
+                  class="capitalize font-black mt-1 text-lg leading-tight font-large text-red-200 group-hover:text-black hover:underline"
+                >
+                  Finding customers for your new business
+                </a>
+                <p class="mt-2 text-red-200 group-hover:text-gray-500">
+                  Getting a new business off the ground is a lot of hard work.
+                  Here are five ideas you can use to find your first customers.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class=" group border-4 border-green-500 hover:bg-white hover:shadow-green hover:border-transparent  relative group max-w-md mx-auto rounded-xl  overflow-hidden md:max-w-2xltransition duration-150 ease-in-out transform hover:scale-110   ">
+            <div class="md:flex">
+              <div class="md:flex-shrink-0">
+                <img
+                  class="h-48 w-full object-cover md:h-full md:w-48"
+                  src="https://via.placeholder.com/350x150"
+                  alt="Man looking at item at a store"
+                ></img>
+              </div>
+              <div class="p-8">
+                <a
+                  href="#"
+                  class="capitalize font-black mt-1 text-lg leading-tight font-large text-green-200 group-hover:text-black hover:underline"
+                >
+                  Finding customers for your new business
+                </a>
+                <p class="mt-2 text-green-200 group-hover:text-gray-500">
+                  Getting a new business off the ground is a lot of hard work.
+                  Here are five ideas you can use to find your first customers.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </ContainerAbout>
+      </div>
+    </>
+  );
+};
+
+export default About;
