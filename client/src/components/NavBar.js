400b880731dc9aa301afe54fe57ffbc489016303
Rishil Saxena <rishilnandysaxena@gmail.com>
2021-08-31 18:50:58 -0400

    .

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -14,8 +14,8 @@ const NavBar = ({ currentPage }) => {
           to="/about"
           className={`${
             currentPage === "/about"
-              ? " text-xl md:rounded bg-gradient-to-r hover:from-green-500 hover:to-blue-400 from-purple-500 to-red-600 text-black md:p-3" //  from-green-500 to-blue-400
-              : "text-xl md:rounded bg-gradient-to-r to-green-400 from-blue-300 hover:bg-gradient-to-r hover:from-purple-500 hover:to-red-600 text-black md:p-3"
+              ? " text-xl md:rounded bg-gradient-to-r hover:from-green-500 hover:to-blue-400 from-purple-500 to-red-600 text-transparent md:p-3 bg-clip-text font-bold" //  from-green-500 to-blue-400
+              : "text-xl md:rounded bg-gradient-to-r to-green-400 from-blue-300 hover:bg-gradient-to-r hover:from-purple-500 hover:to-red-600 text-transparent md:p-3 bg-clip-text font-bold"
           }`}
         >
           About Us
@@ -24,8 +24,8 @@ const NavBar = ({ currentPage }) => {
           to="/program"
           className={`${
             currentPage === "/program"
-              ? " text-xl md:rounded bg-gradient-to-r hover:from-green-500 hover:to-blue-400 from-red-600 to-purple-500 text-black md:p-3" //  from-green-500 to-blue-400
-              : "text-xl md:rounded bg-gradient-to-r to-red-300 from-green-400 hover:bg-gradient-to-r hover:from-red-600 hover:to-purple-500 text-black md:p-3"
+              ? " text-xl md:rounded bg-gradient-to-r hover:from-green-500 hover:to-blue-400 from-red-600 to-purple-500 text-transparent md:p-3 bg-clip-text font-bold" //  from-green-500 to-blue-400
+              : "text-xl md:rounded bg-gradient-to-r to-red-300 from-green-400 hover:bg-gradient-to-r hover:from-red-600 hover:to-purple-500 text-transparent md:p-3 bg-clip-text font-bold"
           }`}
         >
           Program
@@ -35,8 +35,10 @@ const NavBar = ({ currentPage }) => {
         to="/register"
         className={`${
           currentPage === "/register"
-            ? " text-xl md:rounded bg-gradient-to-r hover:from-green-500 hover:to-blue-400 from-purple-500 to-red-600 text-black md:p-3" //  from-green-500 to-blue-400
-            : "text-xl md:rounded bg-red-400 hover:bg-gradient-to-r hover:from-purple-500 hover:to-red-600 text-black md:p-3"
+            ? " text-xl md:rounded bg-gradient-to-r hover:from-green-500 hover:to-blue-400 from-red-600 to-purple-500 text-transparent md:p-3 bg-clip-text font-bold" //  from-green-500 to-blue-400
+            : "text-xl md:rounded bg-gradient-to-r to-red-300 from-green-400 hover:bg-gradient-to-r hover:from-red-600 hover:to-purple-500 text-transparent md:p-3 bg-clip-text font-bold"
+          // ? " text-xl md:rounded bg-gradient-to-r hover:from-green-500 hover:to-blue-400 from-purple-500 to-red-600 text-transparent md:p-3 bg-clip-text" //  from-green-500 to-blue-400
+          // : "text-xl md:rounded bg-red-400 hover:bg-gradient-to-r hover:from-purple-500 hover:to-red-600 text-black md:p-3 bg-clip-text"
         }`}
       >
         Register
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,84 @@
-import React from 'react'
+import React from "react";
+import "./register.css";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-    return (
-        <div>
-            
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    //post data to server
+    console.log(data);
+  };
+  // pattern: /^[0-9]+$/g
+  return (
+    <div className="w-1/2 m-auto text-center bg-gray-800 rounded p-10 mb-20 shadow-xl">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="txt-field">
+          <input
+            className="form-input w-3/4 bg-transparent mb-10 m-auto"
+            {...register("firstName", { required: true })}
+            required
+          />
+          <span className="form-label bg-gradient-to-r from-green-400 to-blue-300 text-transparent bg-clip-text font-bold">
+            First Name
+            {errors.firstName && <p className="inline text-red-600 ml-6">Please enter first name</p>}
+          </span>
         </div>
-    )
-}
+        <div className="txt-field">
+          <input
+            className="form-input w-3/4 bg-transparent mb-10 m-auto"
+            {...register("lastName", { required: true })}
+            required
+          />
+          <span className="form-label bg-gradient-to-r from-green-400 to-blue-300 text-transparent bg-clip-text font-bold">
+            Last Name
+            {errors.lastName && <p className="inline text-red-600 ml-6">Please enter last name</p>}
+          </span>
+        </div>
+        <div className="txt-field">
+          
+          <input
+            className="form-input w-3/4 bg-transparent mb-10 m-auto"
+            {...register("email", {
+              required: true,
+              pattern:
+                /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+            })}
+            required
+          />
+          <span className="form-label bg-gradient-to-r from-green-400 to-blue-300 text-transparent bg-clip-text font-bold">
+            Email
+            {errors.email && <p className="inline text-red-600 ml-6">Invalid email</p>}
+          </span>
 
-export default Register
+        </div>
+        <div className="txt-field">
+          <input
+            className="form-input w-3/4 bg-transparent mb-10 m-auto"
+            {...register("age", {
+              required: true,
+              pattern: /^[0-9]+$/g,
+            })}
+            required
+          />
+          <span className="form-label for-age bg-gradient-to-r from-green-400 to-blue-300 text-transparent bg-clip-text font-bold">
+            Age
+            {errors.age && <p className="inline text-red-600 ml-6">Invalid age</p>}
+          </span>
+        </div>
+        <button
+          type="submit"
+          className="border-0 bg-gradient-to-r from-green-400 to-blue-300 text-black font-bold rounded p-3"
+        >
+          Register now
+        </button>
+      </form> 
+    </div>
+  );
+};
+
+export default Register;
