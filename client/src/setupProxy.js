407f689e2bd1851f7a46e00d3efc42338df035c5
Rishil Saxena <rishilnandysaxena@gmail.com>
2021-09-09 19:11:52 -0400

    bugs and finished confirm page

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const createProxyMiddleware = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    ["/register", "/register/*"], //routes the proxy is looking for
+    ["/api/register", "/api/register/*"], //routes the proxy is looking for
     createProxyMiddleware({
       target: "http://localhost:3001", // proxy will serve data to target
     })
