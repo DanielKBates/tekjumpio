76189c899ade79f4c0e61d7fe379bf20674b0373
Daniel Bates <danielbates95@gmail.com>
2021-11-30 14:37:05 -0500

    FormSite redesign .9

--- src/pages/Register/FormSite.js
+++ src/pages/Register/FormSite.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Switch } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/outline";
 import { dayWeeks, nightWeeks } from "../../utils/schedules";
 import { registerInfo } from "../../utils/registerInfo";
 
@@ -10,6 +11,7 @@ function classNames(...classes) {
 
 const FormSite = () => {
   const [toggle, setToggle] = useState(true);
+  const regRef = useRef();
   const embedForm = () => {
     window.EmbedManager.embed({
       key: "https://fs26.formsite.com/res/showFormEmbed?EParam=m_OmK8apOTASBU8K-Mte1yJ-zpXHYORbFzpUCZwnDno&1948865317",
@@ -22,93 +24,132 @@ const FormSite = () => {
     return () => window.removeEventListener("message", embedForm);
   }, []);
 
+  const handleToRegClick = () => {
+    regRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+
   return (
     <div className="pt-24 mx-auto w-full lg:w-2/3">
       <div className="flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="pt-2 text-lg align-middle inline-block px-2 md:px-8 lg:px-10">
             <div className="shadow overflow-hidden border-b border-gray-200">
-              <div className="flex-col space-y-4 lg:flex-row  bg-indigo-900 px-6 pt-12 pb-2 rounded-tl-lg rounded-tr-lg ">
+              <div className="flex-col space-y-4 lg:flex-row  multiGradient animate-animateGradient px-6 pt-12 pb-2 rounded-tl-lg rounded-tr-lg ">
                 <span className="text-4xl xl:text-5xl font-semibold text-gray-50">
                   Class Details and Registration
                 </span>
               </div>
-              <div className=" bg-gray-50 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
-                {registerInfo.map((element) => (
-                  <div
-                    key={element.title}
-                    className={classNames("relative group bg-gray-50 p-6 ")}
-                  >
-                    <div className="mt-4">
-                      <h3 className="text-2xl text-indigo-500 font-medium">
-                        <div>{element.title}</div>
-                      </h3>
-                      <ul className="list-outside list-disc space-y-2 text-gray-600">
-                        {element.points.map((point, textIndx) => (
-                          <li key={point.id}>{point.text}</li>
-                        ))}
+              <div className="bg-white">
+                <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+                  <div className="pb-16 xl:flex xl:items-center xl:justify-between">
+                    <div>
+                      <h1 className="text-4xl font-extrabold sm:text-5xl sm:tracking-tight">
+                        <span className="text-gray-900">
+                          Expert, remote training for{" "}
+                        </span>
+                        <span className="text-pink-600">$500</span>
+                      </h1>
+                      <p className="mt-5 text-xl text-gray-500">
+                        Jumpstart your skills with our meticulously crafted
+                        curriculum.
+                      </p>
+                    </div>
+                    <button
+                      onClick={handleToRegClick}
+                      className="mt-8 w-full multiGradient animate-animateGradient border border-transparent px-5 py-3 inline-flex items-center justify-center text-base font-medium rounded-md text-white hover:bg-indigo-700 sm:mt-10 sm:w-auto xl:mt-0"
+                    >
+                      Sign up now
+                    </button>
+                  </div>
+                  <div className="border-t border-gray-200 pt-16 xl:grid xl:grid-cols-3 xl:gap-x-8">
+                    <div>
+                      <h2 className="text-base font-semibold text-pink-600 tracking-wide uppercase">
+                        Get trained, Stand out
+                      </h2>
+                      <p className="mt-2 text-3xl font-extrabold text-gray-900">
+                        Your path to a new career
+                      </p>
+                      <p className="mt-4 text-lg text-gray-500">
+                        Complete our 225 hour training program and join our
+                        <span className="text-indigo-600 font-bold"> Graduate Dev Path.</span> Receive continued training, interview
+                        preparation, placement assistance and career coaching.
+                        Work alongside our Dev team -at your own pace- to build
+                        <span className="text-indigo-600 font-bold"> crucial real world experience on live projects for real
+                        clients.</span> You don't pay anything until you've landed your first role as a software developer!
+                      </p>
+                    </div>
+                    <div className="mt-4 sm:mt-8 md:mt-10 md:grid md:grid-cols-2 md:gap-x-8 xl:mt-0 xl:col-span-2">
+                      <ul role="list" className="divide-y divide-gray-200">
+                        {registerInfo.slice(0, 5).map((feature, featureIdx) =>
+                          featureIdx === 0 ? (
+                            <li
+                              key={feature}
+                              className="py-4 flex md:py-0 md:pb-4"
+                            >
+                              <CheckIcon
+                                className="flex-shrink-0 h-6 w-6 text-green-500"
+                                aria-hidden="true"
+                              />
+                              <span className="ml-3 text-base text-gray-500">
+                                {feature}
+                              </span>
+                            </li>
+                          ) : (
+                            <li key={feature} className="py-4 flex">
+                              <CheckIcon
+                                className="flex-shrink-0 h-6 w-6 text-green-500"
+                                aria-hidden="true"
+                              />
+                              <span className="ml-3 text-base text-gray-500">
+                                {feature}
+                              </span>
+                            </li>
+                          )
+                        )}
+                      </ul>
+                      <ul
+                        role="list"
+                        className="border-t border-gray-200 divide-y divide-gray-200 md:border-t-0"
+                      >
+                        {registerInfo.slice(5).map((feature, featureIdx) =>
+                          featureIdx === 0 ? (
+                            <li
+                              key={feature}
+                              className="py-4 flex md:border-t-0 md:py-0 md:pb-4"
+                            >
+                              <CheckIcon
+                                className="flex-shrink-0 h-6 w-6 text-green-500"
+                                aria-hidden="true"
+                              />
+                              <span className="ml-3 text-base text-gray-500">
+                                {feature}
+                              </span>
+                            </li>
+                          ) : (
+                            <li key={feature} className="py-4 flex">
+                              <CheckIcon
+                                className="flex-shrink-0 h-6 w-6 text-green-500"
+                                aria-hidden="true"
+                              />
+                              <span className="ml-3 text-base text-gray-500">
+                                {feature}
+                              </span>
+                            </li>
+                          )
+                        )}
                       </ul>
                     </div>
                   </div>
-                ))}
-                <div className="relative group bg-gray-50 p-6 ">
-                  <div className="mt-4">
-                    <h3 className="text-2xl text-indigo-500 font-medium">
-                      <div>Talk To Us</div>
-                    </h3>
-                    <ul className="list-outside list-disc space-y-2 text-gray-600">
-                      <li>
-                        Jump in our{" "}
-                        <a
-                          className="text-indigo-400"
-                          href="https://us02web.zoom.us/j/89810725704"
-                        >
-                          Meet and Greet Zoom
-                        </a>
-                        , M-F, 1-3pm EST
-                      </li>
-                      <li>
-                        Chat with us anytime on our{" "}
-                        <a
-                          className="text-indigo-400"
-                          href="https://discord.gg/cfQJR85KTG"
-                        >
-                          Q&A Discord Server
-                        </a>{" "}
-                      </li>
-                      <li>
-                        {" "}
-                        Check out our
-                        <Link className="text-indigo-400" to="/contact">
-                          {" "}
-                          Contact page{" "}
-                        </Link>
-                        to talk to us!
-                      </li>
-                    </ul>
-                  </div>
                 </div>
               </div>
-              <p className="text-gray-600 bg-gray-50 font-normal text-lg p-4 ">
-                Our <span className="text-indigo-500 font-bold">225 hour training program </span>is $1,000, due 48 hours
-                within registering for the program. Upon completing our training
-                program, you will be invited to join our <span className="font-bold text-indigo-500">Graduate Career Path,</span>
-                in which you will <span className="font-bold text-indigo-500"> receive continued education, interview
-                preparation, resume building, career coaching,</span> and the
-                opportunity to <span className="font-bold text-indigo-500"> work alongside our dev team to build live,
-                professional sites. </span>
-              </p>
-              <p className="text-gray-600 bg-gray-50 font-normal text-lg p-4 ">
-                We know <span className="font-bold text-indigo-500">life gets busy </span> and that the job hunt can be daunting. We
-                want to work with you - if you join our <span className="font-bold text-indigo-500"> Graduate Career Path, </span> we
-                will <span className="font-bold text-indigo-500">work with your schedule </span> so that you are able to enage in interview preparation sessions, receive  contribute
-                to ongoing development projects and <span className="font-bold text-indigo-500"> build experience at your own
-                pace.</span> Our Graduate Career Path is $2,500, but you <span className="font-bold text-indigo-500">pay nothing until you start your first job in your new career!</span><span className="text-sm align-top">*</span>
-              </p>
 
-              <div className="bg-gray-50 px-4 text-3xl  pt-8 font-semibold text-indigo-600">
-                Class Schedule{" "}
-                <span className="text-lg ml-2">
+              <div className="bg-gray-50 px-4   pt-8 font-semibold text-indigo-600">
+                <span className=" text-pink-600 text-4xl">
+                  {" "}
+                  Class Schedule{" "}
+                </span>
+                <span className="text-pink-600 text-lg ml-2">
                   {toggle ? "(Day)" : "(Night)"}
                 </span>
                 <span>
@@ -118,8 +159,8 @@ const FormSite = () => {
                       checked={toggle}
                       onChange={setToggle}
                       className={classNames(
-                        toggle ? "bg-indigo-700" : "bg-gray-200",
-                        "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-900"
+                        toggle ? "bg-yellow-300 " : "bg-blue-900",
+                        "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none "
                       )}
                     >
                       <span
@@ -133,8 +174,8 @@ const FormSite = () => {
                     <Switch.Label as="span" className="ml-3">
                       <span className="text-sm font-medium text-gray-600">
                         {toggle
-                          ? "See the Night Class Schedule"
-                          : "See the Day Class Schedule"}
+                          ? "See the night class schedule"
+                          : "See the day class schedule"}
                       </span>
                     </Switch.Label>
                   </Switch.Group>
@@ -202,7 +243,9 @@ const FormSite = () => {
           </div>
         </div>
       </div>
-      <a name="form1948865317" id="formAnchor1948865317"></a>{" "}
+      <div ref={regRef}>
+        <a name="form1948865317" id="formAnchor1948865317"></a>{" "}
+      </div>
     </div>
   );
 };
